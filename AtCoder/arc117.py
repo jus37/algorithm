ffbb11f8becd1f12201f416d5f10d3b1b46ab3a8
jus37 <jus_37@outlook.jp>
2021-04-19 17:36:42 +0900

    arc117 B写経AC

--- AtCoder/arc117.py
+++ AtCoder/arc117.py
@@ -29,3 +29,17 @@ else:
     E.append(-n)
   E = [str(n) for n in E]
   print(' '.join(E))
+
+#B
+N = int(input())
+A = list(map(int, input().split()))
+A.sort()
+ans = 1
+former = 0
+for n in range(0, len(A)):
+  ans *= (A[n] - former + 1)
+  ans %= (10**9 + 7)
+  former = A[n]
+
+print(ans)
+
